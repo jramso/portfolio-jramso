88746c61502d8f50fcd38da910c90665297bb8d4
jramso <josue.rsou2@gmail.com>
2025-02-18 01:04:49 -0300

    one last try

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from './views/HomePage.vue'
 import Contato from './views/ContatoPage.vue'
 import Sobre from './views/AboutMe.vue'
@@ -12,7 +12,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(), // Alterado para hash mode
   routes
 })
 
